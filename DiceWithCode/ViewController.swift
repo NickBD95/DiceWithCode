b7ef9b3a8b7918af79f6fff4cc032c229ed9eef6
Nickolai Grachev <nickbd95@gmail.com>
2024-02-05 01:41:49 +0300

    app done

--- DiceWithCode/ViewController.swift
+++ DiceWithCode/ViewController.swift
@@ -7,11 +7,96 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private class ViewController: UIViewController {
+    
+    private let dices = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
+    let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "greenbackground")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "DiceLogo")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let diceOneImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "DiceOne")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let diceTwoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "DiceOne")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var rollButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Roll", for: .normal)
+        button.layer.cornerRadius = 12
+        button.titleLabel?.font = .systemFont(ofSize: 32)
+        button.tintColor = .white
+        button.backgroundColor = UIColor(red: 142/255, green: 41/255, blue: 37/255, alpha: 1)
+        button.addTarget(self, action: #selector(rollButtonPresed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    @objc
+    func rollButtonPresed() {
+        diceOneImageView.image = UIImage(named: dices.randomElement() ?? "DiceOne")
+        diceTwoImageView.image = UIImage(named: dices.randomElement() ?? "DiceOne")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(backgroundImageView)
+
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        view.addSubview(logoImageView)
+        
+        logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        logoImageView.heightAnchor.constraint(equalToConstant: 170).isActive = true
+        logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        logoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    
+        view.addSubview(diceOneImageView)
+        
+        diceOneImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        diceOneImageView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        diceOneImageView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        diceOneImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        
+        view.addSubview(diceTwoImageView)
+        
+        diceTwoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        diceTwoImageView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        diceTwoImageView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        diceTwoImageView.heightAnchor.constraint(equalTo: diceTwoImageView.widthAnchor, multiplier: 1).isActive = true
+        diceTwoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        
+        view.addSubview(rollButton)
+        
+        rollButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        rollButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40).isActive = true
+        rollButton.heightAnchor.constraint(equalToConstant: 65).isActive = true
+        rollButton.widthAnchor.constraint(equalToConstant: 130).isActive = true
     }
 
 
